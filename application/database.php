1e22166eea90aa7d573209bb4a59d3bbe5eae3c1
seqier <seqier@126.com>
2019-05-30 12:14:59 +0800

    配置更新

--- application/database.php
+++ application/database.php
@@ -19,7 +19,7 @@ return [
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'nariims',
+    'password'       => '123456',
     // 端口
     'hostport'       => '3306',
     // 连接dsn
