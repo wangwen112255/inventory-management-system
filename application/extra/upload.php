949f669f2ccbaa51f06f8162a8701e4c3d531d21
seqier <seqier@126.com>
2019-06-19 22:30:11 +0800

    产品添加图片

---
+++ application/extra/upload.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @title 上传配置
+ */
+
+return [
+    'ALLOW_FILE_SIZE'=>2*1024*1024
+];
